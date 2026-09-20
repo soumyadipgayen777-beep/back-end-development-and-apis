67e3f3c9d0fa650e2d6fefabaefb1802537f7878
soumyadipgayen777-beep <soumyadipgayen777@gmail.com>
2026-09-20 10:38:44 +0000

    step 9

--- build-a-file-processor/server.js
+++ build-a-file-processor/server.js
@@ -14,3 +14,4 @@ main();
 fs.writeFileSync("assets/output.txt", "Hello, freeCodeCamp!");
 fs.appendFileSync("assets/output.txt", "\nWelcome to the course");
 console.log(fs.existsSync("assets/output.txt"));
+console.log(fs.readdirSync("assets"));
