03d553eb107640989cc3ef3dab2219e481a9b1bc
soumyadipgayen777-beep <soumyadipgayen777@gmail.com>
2026-09-20 10:29:51 +0000

    step 5

--- build-a-file-processor/server.js
+++ build-a-file-processor/server.js
@@ -3,7 +3,7 @@ const fsPromises = require('fs/promises');
 
 async function main(){
     const data = await fsPromises.readFile('assets/poem.txt', {
-        enconding: 'utf8'
+        encoding: 'utf8'
     });
     console.log(data);
 }
